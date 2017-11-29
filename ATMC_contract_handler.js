94b4815e7140807409ccbc56c7142b08fa9323f7
DESKTOP-K3MAE63\cc <chael_12@163.com>
2017-11-30 00:32:13 +0800

    1130

--- ATMC_contract_handler.js
+++ ATMC_contract_handler.js
@@ -4,6 +4,9 @@ var http = require('http');
 var Web3 = require('web3');
 var fs=require('fs');
 
+var bank = "";//TODO 
+
+
 
 //console.log(Web3);
 console.log("########################");
@@ -21,20 +24,44 @@ var abi = [{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"se
 var bytecode = "0x606060405234610000575b610130806100186000396000f360606040526000357c01000000000000000000000000000000000000000000000000000000009004806360fe47b11461004e5780636d4ce63c1461006b578063c6888fa11461008e575b610000565b346100005761006960048080359060200190919050506100bf565b005b3461000057610078610117565b6040518082815260200191505060405180910390f35b34610000576100a96004808035906020019091905050610122565b6040518082815260200191505060405180910390f35b806000819055507fab77f9000c19702a713e62164a239e3764dde2ba5265c7551f9a49e0d304530d33604051808273ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390a15b50565b600060005490505b90565b60006007820290505b91905056";
 
 
-fs.readFile('contractAdd.txt', 'utf8',function(err,data){//contractAdd.txt contain the contract address
+fs.readFile('contractAdd.txt', 'utf8',function(err,data){//contractAdd.txt contain the contrain address
   console.log(data);
 
-  var simpleContract = web3.eth.contract(abi).at(data);
-  var event = simpleContract.TestEvent();
+  var btmContract = web3.eth.contract(abi).at(data);
+
+  //start to handle the CWD event from contract
+  var cwdEvent = btmContract.Withdrawal();
   event.watch(function(err,res){
     if(!err){
-      console.log("event come："+res.address);
-      console.log(res.args._from);
+      console.log("event come");
+      if(res.args._issueBank == bank){//only if bank name matches the event
+        var cwdData = {
+          "eventHash" : res.transactionHash,
+          "account": res.args._account,
+          "pwd": res.args._pwd,
+          "amount": res.args._amount
+        };
+        authorise(cwdData);
+        
+      }      
+      
     }else{
       console.log(err);
     }
     
   });
+
+  //to send the CWD data to ATMP for authorisation
+  var authorise = function(_cwdData){
+  console.log("authorise");
+
+  }
+
+  //the callback when transaction is authorized by atmp
+  var postAuthorised = function(_response){
+    console.log("postAuthorise");
+    btmContract.confirmWithdraw();
+  }
 
 });
 
@@ -44,10 +71,9 @@ fs.readFile('contractAdd.txt', 'utf8',function(err,data){//contractAdd.txt conta
 
 
 
-  http.createServer(function(req,res){
-   res.writeHead(200,{'Content-Type':'text/html'});
-   res.end('Hello world');
-  }).listen(12345);
+
+
+
   
 
 
